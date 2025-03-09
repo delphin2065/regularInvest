d4b4eb90a351876388e5814e281b41c7c64487f9
delphin2065 <delphin2065@hotmail.com>
2025-03-10 00:09:42 +0800

    first commit

--- regularInvest.py
+++ regularInvest.py
@@ -6,6 +6,7 @@ import streamlit as st
 from matplotlib import pyplot as plt
 import matplotlib
 
+
 plt.rcParams['font.sans-serif']  = ['Taipei Sans TC Beta'] 
 
 st.header('Regular Investment Plan 定期定額投資~~')
